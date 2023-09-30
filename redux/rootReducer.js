4ec63d564a7e20463afe9254949fe1458436c0b8
Vijay <60383996+Coder-Unknown-2106@users.noreply.github.com>
2023-09-30 18:25:46 +0530

    Add files via upload

---
+++ redux/rootReducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import userReducer from "./reducers/UserReducer";
+
+const rootReducer = combineReducers({
+    frontUser: userReducer,
+
+});
+
+export default rootReducer;
---
+++ redux/services/UserService.js
@@ -0,0 +1,30 @@
+import * as apiHelper from '../../helper/ApiHelper'
+
+export const UserService = {
+
+    HomeTime,
+    GetCmsData
+
+}
+
+async function HomeTime(data) {
+    try {
+        const api = await apiHelper.getRequest(`products/1`, data)
+        return api
+    } catch (e) {
+        console.error(e)
+        return e.toString()
+    }
+}
+
+
+async function GetCmsData(data) {
+    try {
+        const api = await apiHelper.postRequest(`products`, data)
+        return api
+    } catch (e) {
+        console.error(e)
+        return e.toString()
+    }
+}
+
